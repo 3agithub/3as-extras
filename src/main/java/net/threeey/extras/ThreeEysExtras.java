073719c2176c7e31d42bf743906c4222fae7b839
3A <74498981+3agithub@users.noreply.github.com>
2024-11-08 21:44:25 +0800

    Version 0.0.1
    Added plastic and raw plastic items.
    
    About Version 0.0.x:
    There will be 1 version increment every time a new feature is added. No releases until version 0.1.0.

--- src/main/java/net/threeey/extras/ThreeEysExtras.java
+++ src/main/java/net/threeey/extras/ThreeEysExtras.java
@@ -2,14 +2,16 @@ package net.threeey.extras;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.threeey.extras.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ThreeEysExtras implements ModInitializer {
-    public static final String MOD_ID = "threeey";
+    public static final String MOD_ID = "teextras";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     @Override
     public void onInitialize() {
+        ModItems.registerModItems();
     }
 }
---
+++ src/main/java/net/threeey/extras/item/ModItems.java
@@ -0,0 +1,31 @@
+package net.threeey.extras.item;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import net.threeey.extras.ThreeEysExtras;
+
+public class ModItems {
+
+    public static final Item PLASTIC = registerItem("plastic", new Item(new Item.Settings()));
+    public static final Item RAW_PLASTIC = registerItem("raw_plastic", new Item(new Item.Settings()));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(ThreeEysExtras.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        ThreeEysExtras.LOGGER.info("Registering mod items for Extras");
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+            entries.add(PLASTIC);
+        });
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+            entries.add(RAW_PLASTIC);
+        });
+    }
+}
